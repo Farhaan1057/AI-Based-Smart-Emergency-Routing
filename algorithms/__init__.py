2fb02f2c7ea9100f9737a3ca8ad88d0a0c2aa014
Muhammad Farhan <mfarhaan1057@gmail.com>
2026-06-13 13:39:00 +0500

    Initialize algorithms package with imports
    Added imports and __all__ for algorithm functions.

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,1 +1,8 @@
+from algorithms.bfs    import bfs
+from algorithms.dfs    import dfs
+from algorithms.greedy import greedy
+from algorithms.astar  import astar
 
+__all__ = ["bfs", "dfs", "greedy", "astar"]
+
+# for making the algos a pkg
